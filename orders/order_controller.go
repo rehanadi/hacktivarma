7370306288b9849a00a1047c6cfe36f0900048c8
Rehan Adi Purwana <rehanadipurwana@gmail.com>
2024-11-22 00:21:27 +0700

    feat: add order date in get all orders

--- orders/order_controller.go
+++ orders/order_controller.go
@@ -29,13 +29,13 @@ func (oc *OrderController) GetAllOrders(userId interface{}) {
 	}
 
 	screenLine(width)
-	fmt.Printf("%-8s | %-14s | %-14s | %-8s | %-8s | %-12s | %-14s | %-14s | %-20s | %-15s | %-20s\n",
-		"ID", "User", "Drug", "Quantity", "Price", "Total", "Payment Method", "Payment Status", "Payment At", "Delivery Status", "Delivered At")
+	fmt.Printf("%-8s | %-14s | %-14s | %-8s | %-8s | %-12s | %-20s | %-14s | %-14s | %-20s | %-15s | %-20s\n",
+		"ID", "User", "Drug", "Quantity", "Price", "Total", "Order At", "Payment Method", "Payment Status", "Payment At", "Delivery Status", "Delivered At")
 	screenLine(width)
 
 	for _, order := range allOrders {
-		fmt.Printf("%-8v | %-14v | %-14v | %-8v | Rp %-8.0f | Rp %-12.0f | %-14v | %-14v | %-20v | %-15v | %-20v\n",
-			order.Id, order.UserName, order.DrugName, order.Quantity, order.Price*1000, order.TotalPrice*1000, order.PaymentMethod, order.PaymentStatus, order.PaymentAt.Format("2006-01-02"), order.DeliveryStatus, order.DeliveredAt.Format("2006-01-02"))
+		fmt.Printf("%-8v | %-14v | %-14v | %-8v | Rp %-8.0f | Rp %-12.0f | %-20v | %-14v | %-14v | %-20v | %-15v | %-20v\n",
+			order.Id, order.UserName, order.DrugName, order.Quantity, order.Price*1000, order.TotalPrice*1000, order.CreatedAt.Format("2006-01-02"), order.PaymentMethod, order.PaymentStatus, order.PaymentAt.Format("2006-01-02"), order.DeliveryStatus, order.DeliveredAt.Format("2006-01-02"))
 	}
 
 	screenLine(width)
