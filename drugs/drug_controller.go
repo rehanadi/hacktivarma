264beef88f9b6b0036ea75d9c753d90308d7d9f2
Kito <surya.aw26@gmail.com>
2024-11-21 19:33:42 +0700

    refactor drug controller

--- drugs/drug_controller.go
+++ drugs/drug_controller.go
@@ -18,61 +18,58 @@ func screenLine(width int) {
 	for i := 0; i < width; i++ {
 		fmt.Printf("-")
 	}
+
 	fmt.Println("")
 }
 
 func (dc *DrugController) GetAllDrugs() {
 	width := 64
 	allDrugs, err := dc.DrugService.GetAllDrugs()
+
 	if err != nil {
 		fmt.Println("Error :", err)
 	}
 
 	screenLine(width)
-	fmt.Printf("%-8s | %-14s | %-5v | %-11s | %-14v\n", "ID", "Drug Name", "Stock", "Price", "Expired")
+	fmt.Printf("%-8s | %-14s | %-5v | %-11s | %-14s\n", "ID", "Drug Name", "Stock", "Price", "Expired")
 	screenLine(width)
 
 	for _, drug := range allDrugs {
-		fmt.Printf("%-8v | %-14v | %-5v | Rp %-8.0f | %-14v\n", drug.Id, drug.Name, drug.Stock, drug.Price*1000, drug.ExpiredDate.Format("2006-01-02"))
+		fmt.Printf("%-8s | %-14s | %-5v | Rp %-8.0f | %-14s\n", drug.Id, drug.Name, drug.Stock, drug.Price*1000, drug.ExpiredDate.Format("2006-01-02"))
 	}
 
 	screenLine(width)
-
-	//
-
 }
 
-func (dc *DrugController) AddDrug(
-	// name string, dose float64, form string, stock int, price float64, expired_date string, category int,
-	drug entity.Drug,
-) error {
-	err := dc.DrugService.AddDrug(
-		// name, dose, form, stock, price, expired_date, category,
-		drug.Name, drug.Dose, drug.Form, drug.Stock, drug.Price, drug.ExpiredDate.Format("2006-01-02"), drug.Category,
-	)
+func (dc *DrugController) AddDrug(drug entity.Drug) error {
+	err := dc.DrugService.AddDrug(drug)
+
 	if err != nil {
-		fmt.Println("Error :", err)
+		fmt.Println("Error:", err)
 		return err
 	}
-	fmt.Println("Drug Created")
+
+	fmt.Println("Drug Created Successfully")
 	return nil
 }
 
 func (dc *DrugController) UpdateDrugStock(drugId string, updatedStock int) {
-
 	err := dc.DrugService.UpdateDrugStock(drugId, updatedStock)
+
 	if err != nil {
 		fmt.Println("Error update stock :", err)
 		return
 	}
+
 	fmt.Println("Update success :", drugId, updatedStock)
 }
 
 func (dc *DrugController) DeleteDrugById(drugId string) {
-
 	err := dc.DrugService.DeleteDrugById(drugId)
 
 	if err != nil {
 		fmt.Println("Error delete drug :", err)
 	}
+
+	fmt.Printf("Successfully delete drug with id: %s :", drugId)
 }
